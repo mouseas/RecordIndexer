65c0ebd0233f1a450647d22c5a14d7137bb0fb23
Martin Carney <mouseasw@gmail.com>
2012-10-29 21:11:09 -0600

    Tried some things with sockets. Starting with HTTP server.

--- src/server/Server.java
+++ src/server/Server.java
@@ -1,67 +1,13 @@
 package server;
 
-import server.dataAccess.*;
-
 public class Server {
 
-	// Need to add functions to send/receive with ServerCommunicator.
-	
-	private DataAccess database;
-	
 	/**
-	 * Starts the server running on a port defined by the first command-line argument.
-	 * @param args args[0] should be a port number from 0 to 65535
+	 * @param args
 	 */
 	public static void main(String[] args) {
-		if (args.length == 1) {
-			int portNum = -1;
-			Server s = null;
-			try {
-				portNum = Integer.parseInt(args[0]);
-				if (portNum < 0 || portNum > 65535) { throw new NumberFormatException(); }
-				s = new Server();
-				s.run(portNum);
-			} catch (NumberFormatException e) {
-				System.out.println("portNumber must be an integer between 0 and 65535.");
-				usage();
-			}
-		} else {
-			usage();
-		}
+		// TODO Auto-generated method stub
+
 	}
-	
-	private static void usage() {
-		System.out.println("USAGE: java Server portNumber");
-	}
-	
-	/**
-	 * Drops all the current database data and imports data from a saved 
-	 * xml database file using a DataImporter object.
-	 * @param location Location of the database to load
-	 * @return Whether the import was successful
-	 */
-	public boolean importData (String location) {
-		return false;
-	}
-	
-	/**
-	 * Saves the current database data to file using a DataExporter object.
-	 * @param location Location to save the database to. Overwrites any
-	 * existing database data at this location.
-	 * @return Whether the export was successful
-	 */
-	public boolean exportData (String location) {
-		return false;
-	}
-	
-	/**
-	 * Runs the server, receiving and sending messages with clients.
-	 */
-	public void run(int portNum) {
-		
-	}
-	
-	
-	
 
 }
---
+++ src/server/ServerOld.java
@@ -0,0 +1,162 @@
+package server;
+
+import java.io.*;
+import java.net.*;
+import java.util.*;
+
+import server.dataAccess.*;
+
+public class ServerOld {
+
+	// Need to add functions to send/receive with ServerCommunicator.
+	
+	ServerSocket listenSocket;
+	byte[] buffer;
+
+	private DataAccess database;
+	private int port;
+	
+	private static final String DATABASE_LOCATION = 
+		"database" + File.separator + "indexer-app.sqlite";
+	private static final String ASSETS_LOCATION = "serverAssets/";
+	
+	/**
+	 * Constructor. Requires a live DataAccess object in order to access the database.
+	 * @param database
+	 */
+	public ServerOld(DataAccess database) {
+		this.port = -1;
+		this.database = database;
+		
+		listenSocket = null;
+		buffer = new byte[1024];
+	}
+	
+	/**
+	 * Starts the server running on a port defined by the first command-line argument.
+	 * @param args args[0] should be a port number from 0 to 65535
+	 */
+	public static void main(String[] args) {
+		if (args.length == 1) {
+			int portNum = Integer.parseInt(args[0]);
+			ServerOld s = null;
+			try {
+				portNum = Integer.parseInt(args[0]);
+				if (portNum < 0 || portNum > 65535) { throw new NumberFormatException(); }
+				s = new ServerOld(new DataAccess(DATABASE_LOCATION));
+				s.run(portNum);
+			} catch (NumberFormatException e) {
+				System.out.println("portNumber must be an integer between 0 and 65535.");
+				usage();
+			} catch (ServerException e) {
+				System.out.println("Unable to properly connect to database. Terminating.");
+				System.out.println(e.getMessage());
+			}
+		} else {
+			usage();
+		}
+	}
+	
+	private static void usage() {
+		System.out.println("USAGE: java Server portNumber");
+	}
+	
+	/**
+	 * Runs the server, receiving and sending messages with clients.
+	 */
+	public void run(int portNum) {
+		port = portNum;
+		try {
+			listenSocket = new ServerSocket(port);
+			while(acceptAndProcessInput()); // loop until the server receives an "exit" command
+		} catch (IOException e) {
+			e.printStackTrace();
+		} finally {
+			safeClose(listenSocket);
+		}
+		
+	}
+	
+	/**
+	 * Gets the first line of a request, and sorts the request to the relevant functions.
+	 * @return
+	 * @throws IOException
+	 */
+	private boolean acceptAndProcessInput() throws IOException {
+		Socket clientSocket = listenSocket.accept();
+		try {
+			Scanner input = new Scanner(clientSocket.getInputStream());
+			
+			String command = input.nextLine();
+			System.out.println(command);
+			
+			if (command.startsWith("exit")) {
+				return false; // Kill the server.
+			} else if (command.startsWith("get")) {
+				// Get a file, such as an image, help text, or recognized entries.
+				getFile(clientSocket, command);
+			} else {
+				// pretty much everything else.
+				
+			}
+		} catch (IOException e) {
+			System.out.println(e.getMessage());
+			e.printStackTrace();
+		} finally {
+			safeClose(clientSocket);
+		}
+		return true;
+	}
+	
+	/**
+	 * Sends the requested file to the client, such as an image, or help text.
+	 * All files requested must exist in the /serverAssets directory.
+	 * @param clientSocket Established socket with a client.
+	 */
+	private void getFile(Socket clientSocket, String firstLine) throws IOException {
+		Scanner input = new Scanner(clientSocket.getInputStream());
+		PrintWriter output = new PrintWriter(clientSocket.getOutputStream());
+		
+		String filePath = ASSETS_LOCATION + firstLine.substring("get ".length());
+		InputStream file = null;
+		
+		try {
+			file = new BufferedInputStream(new FileInputStream(filePath));
+		} catch (IOException e) {
+			output.print("error " + e.getMessage() + "\n");
+			output.flush();
+		}
+		
+		if (file != null) {
+			try {
+				output.print("ok\n");
+				output.flush();
+
+				int bytes;
+				while ((bytes = file.read(buffer)) >= 0) {
+					clientSocket.getOutputStream().write(buffer, 0, bytes);
+				}
+			} catch (IOException e) {
+				// Reading the file failed, so give up
+			} finally {
+				safeClose(file);
+			}
+		}
+	}
+	
+	/**
+	 * Closes a variety of objects, usually sockets.
+	 * @param obj
+	 */
+	private void safeClose(Closeable obj) {
+		try {
+			obj.close();
+		} catch (IOException e) {
+			// do nothing; there is nothing we can do.
+		}
+	}
+	
+	
+	
+
+}
