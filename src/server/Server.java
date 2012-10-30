166f3f3bf1c481b9cd8bc98c47173167a0b65dfd
Martin Carney <mouseasw@gmail.com>
2012-10-29 22:12:21 -0600

    Found the right package, com.sun.net.httpsever, but not sure how to use it yet.

--- src/server/Server.java
+++ src/server/Server.java
@@ -1,13 +1,40 @@
 package server;
 
+import java.io.*;
+import java.net.*;
+
+import com.sun.net.httpserver.*;
+
 public class Server {
 
-	/**
-	 * @param args
-	 */
+	
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		try {
+			int port = 8080;
+			HttpServer server = HttpServer.create(new InetSocketAddress(port), 10);
+			server.createContext("/serverAssets", new myHandler());
+			server.setExecutor(null); // creates default executor
+			server.start();
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+	}
+	
+	static class myHandler implements HttpHandler {
+		
+		public void handle(HttpExchange t) throws IOException {
+			InputStream is = t.getRequestBody();
+			read(is); // read the request body
+			String response = "This is the response";
+			t.sendResponseHeaders(200, response.length());
+			OutputStream os = t.getResponseBody();
+			os.write(response.getBytes());
+			os.close();
+		}
+		
+		private void read(InputStream is) {
+			
+		}
 	}
 
 }
